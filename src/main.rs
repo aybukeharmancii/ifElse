f2fb9b7e76ffd5bfeeef5fc3b9a59805cd55c905
vrgsl <aybuke.harmanci@virgosol.com>
2022-10-09 20:44:14 +0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let n =55;
+
+    if n == 30 {
+        println!("The number is 30!");
+    } 
+    else if n > 50{
+        println!("The number is greater than 50!")
+    }
+    else {
+        println!("The number is not 30!")
+    }
+}
